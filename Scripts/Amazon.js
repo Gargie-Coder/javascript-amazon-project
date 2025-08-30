9f7ab384e70ddc059618b008fc28b2f3e3acca4a
Gargie Gupta <gargie.info@gmail.com>
2025-08-30 23:27:23 +0530

    Use products.js file

--- Scripts/Amazon.js
+++ Scripts/Amazon.js
@@ -1,9 +1,6 @@
-const Products=[{image:"images/products/athletic-cotton-socks-6-pairs.jpg",name:"Black and Gray Athletic Cotton Socks - 6 Pairs",price:"1090",Rating:{stars:4.5,count:87}},{image:"images/products/intermediate-composite-basketball.jpg",name:"Intermediate Size Basketball",price:"2095",Rating:{stars:4,count:127}},{image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",name:"Adults Plain Cotton T-Shirt - 2 Pack, Light Teal",price:"1995",Rating:{stars:4.5,count:93}}];
 
 let productsHTML="";
-
-
-Products.forEach((product)=>{productsHTML+=`<div class="product-container">
+products.forEach((product)=>{productsHTML+=`<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
@@ -15,14 +12,14 @@ Products.forEach((product)=>{productsHTML+=`<div class="product-container">
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.Rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-              ${product.Rating.count}
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${product.price/100}
+            $${product.priceCents/100}
           </div>
 
           <div class="product-quantity-container">
