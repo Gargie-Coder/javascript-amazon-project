4e60cb6dc1fbde4b916a5e47d9a429828a9fc9a9
Gargie Gupta <gargie.info@gmail.com>
2026-01-23 17:44:34 +0530

    Add backend practice file

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderCheckoutHeader } from "./checkout/checkoutheader.js";
 import {renderOrderSummary} from "./checkout/ordersummary.js";
 import {renderpaymentsummary} from "./checkout/paymentsummary.js";
 import Cart from "../data/cart-class.js";
-
+import  "../data/backend-practice.js";
 renderOrderSummary();
 renderpaymentsummary();
 renderCheckoutHeader();
